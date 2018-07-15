ca1ee7d9231cf8669184fc7c70139795e78fe4ac
Eric Woods <ewoods@ithaca.edu>
2018-07-15 02:28:11 -0400

    Retrieving terminus_env

---
+++ .circleci/scripts/terminus-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Default environment uses the CircleCI build num.
+TERMINUS_ENV=ci-$CIRCLE_BUILD_NUM
+
+# If there is a PR number provided, use it instead.
+if [[ -n ${CIRCLE_PULL_REQUEST} ]] ; then
+  TERMINUS_ENV=pr-${CIRCLE_PULL_REQUEST##*/}
+fi
+
+# But if this is the deployment branch, we use that.
+if [[ $CIRCLE_BRANCH == $DEPLOYMENT_BRANCH ]]
+then
+  TERMINUS_ENV=ci-dev-$CIRCLE_BUILD_NUM
+fi
+
+export $TERMINUS_ENV
