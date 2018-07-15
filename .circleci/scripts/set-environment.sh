4de846284e1b1d2b38e139cae737a6243a107e62
Eric Woods <ewoods@ithaca.edu>
2018-07-15 00:43:00 -0400

    Persisting workspace

--- .circleci/scripts/set-environment.sh
+++ .circleci/scripts/set-environment.sh
@@ -7,12 +7,9 @@ set -ex
 #export CI_LABEL=ci-$CIRCLE_BUILD_NUM
 #export ENV=$(echo ${PR_LABEL:-$CI_LABEL} | cut -c -11 | sed 's/-$//')
 
-(
-    echo "export TEST=simple"
-    echo "export PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f7)"
-    echo "export PR_LABEL=$CIRCLE_BRANCH:+pr-$PR_NUMBER}"
-    echo "export CI_LABEL=ci-$CIRCLE_BUILD_NUM"
-    echo "export ENV=$(echo ${PR_LABEL:-$CI_LABEL} | cut -c -11 | sed 's/-$//')"
-) > env/.environment
-
-bash env/.environment
+mkdir -p env
+echo "export TEST=simple" >> env/.environment
+echo "export PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f7)" >> env/.environment
+echo "export PR_LABEL=$CIRCLE_BRANCH:+pr-$PR_NUMBER}" >> env/.environment
+echo "export CI_LABEL=ci-$CIRCLE_BUILD_NUM" >> env/.environment
+echo "export ENV=$(echo ${PR_LABEL:-$CI_LABEL} | cut -c -11 | sed 's/-$//')" >> env/.environment
