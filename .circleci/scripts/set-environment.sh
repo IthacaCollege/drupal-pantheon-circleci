4135d2f72786afcea00982246b617daa8e7ff2ea
Eric Woods <ewoods@ithaca.edu>
2018-07-15 00:01:12 -0400

    More env setting

--- .circleci/scripts/set-environment.sh
+++ .circleci/scripts/set-environment.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-export PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f7)
-export PR_LABEL=$CIRCLE_BRANCH:+pr-$PR_NUMBER}
-export CI_LABEL=ci-$CIRCLE_BUILD_NUM
-export ENV=$(echo ${PR_LABEL:-$CI_LABEL} | cut -c -11 | sed 's/-$//')
+set -ex
+
+#export PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f7)
+#export PR_LABEL=$CIRCLE_BRANCH:+pr-$PR_NUMBER}
+#export CI_LABEL=ci-$CIRCLE_BUILD_NUM
+#export ENV=$(echo ${PR_LABEL:-$CI_LABEL} | cut -c -11 | sed 's/-$//')
+
+(
+    echo "export TEST=simple"
+    echo "export PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f7)"
+    echo "export PR_LABEL=$CIRCLE_BRANCH:+pr-$PR_NUMBER}"
+    echo "export CI_LABEL=ci-$CIRCLE_BUILD_NUM"
+    echo "export ENV=$(echo ${PR_LABEL:-$CI_LABEL} | cut -c -11 | sed 's/-$//')"
+) >> $BASH_ENV
+
+source $BASH_ENV
