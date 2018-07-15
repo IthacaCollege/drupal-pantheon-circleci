e709563cb47a67109b28de011a9bd19f255b52ad
Eric Woods <ewoods@ithaca.edu>
2018-07-15 01:07:34 -0400

    Persisting workspace

--- .circleci/scripts/set-environment.sh
+++ .circleci/scripts/set-environment.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-set -ex
+mkdir -p config
+ENV_FILE=config/env.sh
 
-#export PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f7)
-#export PR_LABEL=$CIRCLE_BRANCH:+pr-$PR_NUMBER}
-#export CI_LABEL=ci-$CIRCLE_BUILD_NUM
-#export ENV=$(echo ${PR_LABEL:-$CI_LABEL} | cut -c -11 | sed 's/-$//')
-
-mkdir -p env
-echo "export TEST=simple" >> env/.environment
-echo "export PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f7)" >> env/.environment
-echo "export PR_LABEL=$CIRCLE_BRANCH:+pr-$PR_NUMBER}" >> env/.environment
-echo "export CI_LABEL=ci-$CIRCLE_BUILD_NUM" >> env/.environment
-echo "export ENV=$(echo ${PR_LABEL:-$CI_LABEL} | cut -c -11 | sed 's/-$//')" >> env/.environment
+echo "export TEST=simple" >> $ENV_FILE
+echo "export PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f7)" >> $ENV_FILE
+echo "export PR_LABEL=$CIRCLE_BRANCH:+pr-$PR_NUMBER}" >> $ENV_FILE
+echo "export CI_LABEL=ci-$CIRCLE_BUILD_NUM" >> $ENV_FILE
+echo "export ENV=$(echo ${PR_LABEL:-$CI_LABEL} | cut -c -11 | sed 's/-$//')" >> $ENV_FILE
